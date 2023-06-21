d7dbe79ff64d3824997d5b81386ab42ee9ab0be0
YusefNsar <yousefnasar912333@gmail.com>
2023-06-21 05:07:01 +0300

    chore: upload leatest tests

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 from openbabel import pybel
 import os
 import sys
+import pickle
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,15 +17,6 @@ from model.density_transformer import DensityTransformer
 from model.PUResNet import PUResNet
 
 
-def make_output_folder(output_folder_path, input_folder_path):
-    protein_name = os.path.basename(input_folder_path)
-    o_path = os.path.join(output_folder_path, protein_name)
-    # print(o_path)
-    if not os.path.exists(o_path):
-        os.mkdir(o_path)
-    return o_path
-
-
 mol = next(pybel.readfile("mol2", "test/1a2n_1/protein.mol2"))
 
 # o_path = make_output_folder("test/output", "test/1a2n_1/protein.mol2")
@@ -34,14 +26,26 @@ mol = next(pybel.readfile("mol2", "test/1a2n_1/protein.mol2"))
 # print(fe.get_feature(mol)[1][0])
 # print(fe.FEATURE_NAMES)
 
+# dt = DensityTransformer(max_dist=35.0, scale=0.5)
+# grid = dt.setMol(mol).transform()
+
+# d = dt.box_size
+# f = len(dt.fe.FEATURE_NAMES)
+# model = PUResNet(d, f)
+# model.load_weights("/home/yusef/Development/FCI/PUResNet/whole_trained_model1.hdf")
+# model.summary()
+# x = model.predict(grid)
+# print(x.sum(), x)
+
+# with open("prediction.pickle", "wb") as f:
+#     pickle.dump(x, f)
+
+x = None
+with open("prediction.pickle", "rb") as f:
+    x = pickle.load(f)
 
 dt = DensityTransformer(max_dist=35.0, scale=0.5)
 grid = dt.setMol(mol).transform()
 
-d = dt.box_size
-print(d)
-f = len(dt.fe.FEATURE_NAMES)
-model = PUResNet(d, f)
-model.summary()
-x = model.predict(grid)
-print(x.shape, x)
+pockets = dt.segment_grid_to_pockets(x).get_pockets_mols()
+print(pockets, len(pockets[0].atoms))
