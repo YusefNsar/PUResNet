59b4f815c84637b488ef0862786c3a700b7f4198
YusefNsar <yousefnasar912333@gmail.com>
2023-06-25 04:28:19 +0300

    feat: put mol_grid and model in object

--- model/model_runner.py
+++ model/model_runner.py
@@ -3,30 +3,31 @@ from typing import List
 from openbabel import pybel
 from utils.mol_3d_grid import Mol3DGrid
 from model.PUResNet import PUResNet
+import numpy as np
 
 
 class ModelRunner:
     def __init__(self) -> None:
+        self.mol_grid = Mol3DGrid(max_dist=35.0, scale=0.5)
+        d = self.mol_grid.box_size
+        f = len(self.mol_grid.fe.FEATURE_NAMES)
+
+        # get box size and feature number to determine input shape to model
+        self.model = PUResNet(d, f)
+
         pass
 
     def predictBindingSites(self, mol: pybel.Molecule) -> List[pybel.Molecule]:
-        mol_grid = Mol3DGrid(max_dist=35.0, scale=0.5)
-
         # save mol in grid with the required max_dist and scaling
-        grid = mol_grid.setMol(mol).transform()
-
-        # get box size and feature number to determine input shape to model
-        d = mol_grid.box_size
-        f = len(mol_grid.fe.FEATURE_NAMES)
-        model = PUResNet(d, f)
+        grid = self.mol_grid.setMol(mol).transform()
 
         # load trained weights
-        model.load_weights(
+        self.model.load_weights(
             "/home/yusef/Development/FCI/PUResNet/whole_trained_model1.hdf"
         )
 
         # predict and extract predicted pockets
-        x = model.predict(np.array([grid]))
-        pockets = mol_grid.segment_grid_to_pockets(x).get_pockets_mols()
+        x = self.model.predict(np.array([grid]))
+        pockets = self.mol_grid.segment_grid_to_pockets(x).get_pockets_mols()
 
         return pockets
